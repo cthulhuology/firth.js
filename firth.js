c38f1cef5376dca21142f7b241832baaec07fd86
Dave Goehrig <dgoehrig@critical-media.com>
2012-10-08 16:03:53 -0400

    accounting for all of the optmizations:
    2x safari
    7x chrome
    3x firefox

--- firth.js
+++ firth.js
@@ -17,7 +17,7 @@ long = new Uint32Array(Memory)
 dump = function(offset, size) { 
 	var range = []
 	for (var i = 0; i < size; ++i) range.push(offset + i)
-	return range.map(function(x) { return Memory.long[x] })
+	return range.map(function(x) { return long[x] })
 }
 
 sp = 0
@@ -25,9 +25,10 @@ stmp = 0
 rp = 0
 rtmp = 0
 
-
 // preoptimization: Compiler.eval('1600000 push 0 ~ : loop 1 +  <- loop') on safari took 1 second, .6 seconds on chrome, 10s on FF aurora
-// postoptimization:
+// 			or about 4.8mips safari, 8mips chrome, .5mips ff 
+// postoptimization: Compiler.eval('1600000 push 0 ~ : loop 1 +  <- loop') on safari took 0.5 seconds, 0.085 seconds, 3.6s on FF aurora
+//			or about 9.7mips safari, 56mips chrome, 1.3mips ff
 
 main = function () {
 	var a = 0;		// source register
@@ -43,10 +44,8 @@ fetch: while(true) {
 			console.log("Run Time: ", (stop - start) / 1000, "s")
 			console.log("Stack Trace:");
 			console.log("IP: ", i, " Source: ", a, " Destination: ", d)
-			console.log("SP: ", sp, " TMP: ", stmp, " Data: ", 
-			[ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31].map(function(x) { return int[x] }))
-			console.log("RP: ", rp, " TMP: ", rtmp, " Data: ", 
-			[ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31].map(function(x) { return int[x+32] }))
+			console.log("SP: ", sp, " TMP: ", stmp, " Data: ", dump(0,32))
+			console.log("RP: ", rp, " TMP: ", rtmp, " Data: ", dump(32,32))
 			return int[(sp&31)];
 		}
 		var instr = long[i++];			// post increment instruction pointer after reading the instruction 
@@ -105,11 +104,11 @@ Compiler = {
 		';','<->','->','#','$',',','?0','?','<-', ],
 	definitions: {},
 	define: function(word,value) {
-		console.log("Define ", word, " = ", value )
+		// console.log("Define ", word, " = ", value )
 		Compiler.definitions[word] = value
 	},
 	lookup: function(word) {
-		console.log("Lookup ", word)
+		// console.log("Lookup ", word)
 		return Compiler.definitions.hasOwnProperty(word) ?
 			Compiler.definitions[word] :
 			parseInt(word)
@@ -121,9 +120,9 @@ Compiler = {
 		var lit = Compiler.instructions.indexOf('#')						// we use this opcode for literal values
 		while (words.length) {
 			var word = words.shift()							// consume next word in source
-			console.log("compiling ", word, " at ", offset, ":", count)
+			// console.log("compiling ", word, " at ", offset, ":", count)
 			if (word == ':' ) {
-				if (count != 0) Memory.long[offset++] = instr				// align to a cell boundary
+				if (count != 0) long[offset++] = instr					// align to a cell boundary
 				instr = 0
 				count = 0								// reset count to 0
 				Compiler.define(words.shift(),offset)					// define this offset as next word in source 
@@ -132,14 +131,14 @@ Compiler = {
 			var op = Compiler.instructions.indexOf(word)					// -1 is not an instruction
 			if ( op < 0 ) {									// test to see if constant or definition
 				instr += lit << (5*count)
-				Memory.long[offset++] = instr						// save the current instruction long
+				long[offset++] = instr							// save the current instruction long
 				instr = Compiler.lookup(word)						// compile the literal value
 				count = 0								// zero the count so we advance the offset
 			} else {									// this is an instruction
-				console.log("opcode: ", op)
+				// console.log("opcode: ", op)
 				instr += op << (5*count)		
 				if (op > 25) {								// 26+ have literal values following
-					Memory.long[offset++] = instr					// save the current packed instruction
+					long[offset++] = instr						// save the current packed instruction
 					instr = Compiler.lookup(words.shift())				// consume the next word and lookup literal
 					count = 0							// reset count so we save literal value
 				} else {
@@ -147,11 +146,11 @@ Compiler = {
 				}
 			}
 			if (count == 0) {
-				Memory.long[offset++] = instr						// save the current instruction word
+				long[offset++] = instr							// save the current instruction word
 				instr = 0								// reset the instruction
 			}
 		}
-		Memory.long[offset] = instr								// save the last instruction word
+		long[offset] = instr									// save the last instruction word
 	},
 	eval: function(line) {
 		Compiler.compile(line.trim().split(/\s+/))
